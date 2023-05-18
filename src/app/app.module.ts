d8ad4bdc2ca2a788ddcafc0ee2a1d8ce006d9e79
Jhindosh <dharshanpillay@gmail.com>
2023-05-18 15:54:10 +0200

    Did the Dash Panel
    Links need to be inserted when the pages have been created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashPanelComponent } from './dash-panel/dash-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashPanelComponent
   ],
   imports: [
     BrowserModule,
