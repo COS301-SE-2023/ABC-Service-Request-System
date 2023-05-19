fbee937dc27a853ce23593803a626d926d72c3c6
Jhindosh <dharshanpillay@gmail.com>
2023-05-19 19:39:30 +0200

    the rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketItemComponent } from './ticket-item/ticket-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketItemComponent
   ],
   imports: [
     BrowserModule,
