23097113b277b469d3461557939829f5279a6281
Priyul <Priyul20@gmail.com>
2023-05-20 21:58:50 +0200

    💄 improved css & routing

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,5 +1,6 @@
 export const tickets = [
-  {id: 1, title: 'Update UI and bug fixes', assignee: 'Jesse', reviewer: 'Jaimen', company: 'Hyperion Tech', priority: 'High', startDate: '19/05/2023', endDate: '23/05/2023', status: 'Completed'},
-  {id: 2, title: 'testing tickets', assignee: 'Jesse', reviewer: 'Jaimen', company: 'Hyperion Tech', priority: 'High', startDate: '19/05/2023', endDate: '23/05/2023', status: 'Completed'},
-  // more tickets...
+  {id: 1, title: 'Update UI and bug fixes', assignee: 'Jesse', reviewer: 'Jaimen', company: 'Hyperion Tech', priority: 'High', startDate: '19/05/2023', endDate: '23/05/2023', status: 'DONE'},
+  {id: 2, title: 'testing tickets', assignee: 'Jesse', reviewer: 'Jaimen', company: 'Hyperion Tech', priority: 'High', startDate: '19/05/2023', endDate: '23/05/2023', status: 'DONE'},
+  // write script to generate more tickets of the form later
+  //empyu search returns all -> add later
 ];
