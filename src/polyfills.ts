0b646d165e954f0ee37ecb18a92bacfbea397607
Jaimen789 <jaimengovender26@gmail.com>
2023-05-21 10:24:37 +0200

    Hopefully

---
+++ src/polyfills.ts
@@ -0,0 +1,4 @@
+// This file includes polyfills needed for your application to work properly in modern browsers
+
+import 'zone.js/dist/zone';  // Included for Angular's zone.js
+import 'core-js/es';          // Included for ES6+ browser compatibility
