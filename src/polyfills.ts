29a0a36bee5c07a7e71b40db2cfee1d5b2d60af6
Jaimen789 <jaimengovender26@gmail.com>
2023-05-21 10:32:18 +0200

    Try revert

--- src/polyfills.ts
+++
@@ -1,4 +0,0 @@
-// This file includes polyfills needed for your application to work properly in modern browsers
-
-import 'zone.js/dist/zone';  // Included for Angular's zone.js
-import 'core-js/es';          // Included for ES6+ browser compatibility
