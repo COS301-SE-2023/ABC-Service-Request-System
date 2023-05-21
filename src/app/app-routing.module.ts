a0de184c7e66e5fcedef4a69e864f93b60f6fa85
Priyul <Priyul20@gmail.com>
2023-05-22 00:07:06 +0200

    🚑 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'dashboard', component: AppComponent },
+  { path: '**', redirectTo: '/login' } // fallback
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
