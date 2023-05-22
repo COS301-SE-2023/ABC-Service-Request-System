6b67a2f4734d06f24637115aa50ef978fed97a6f
raamNnoodlez <ashirbutt998@gmail.com>
2023-05-22 10:22:40 +0200

    Merge fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tickets } from './data'
+import { tickets } from './data';
 import { Router } from '@angular/router';
 
 export interface Ticket {
