d8ea62e5516c71232a42caf88deb2bfe154ae29d
Priyul <Priyul20@gmail.com>
2023-05-22 02:53:43 +0200

    🚨 bug fixes

--- src/app/page-header/page-header.component.ts
+++ src/app/page-header/page-header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { tickets } from '../data'
+import { Ticket } from '../app.component';
 
 @Component({
   selector: 'app-page-header',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-header.component.scss']
 })
 export class PageHeaderComponent {
+  tickets = tickets;
+  searchTerm = '';
+  filteredTickets = this.tickets;
+  showForm = false;
 
+  @Output() openForm = new EventEmitter<void>();
+  // @Input() tickets: any[] = [];
+
+  constructor(private router: Router) {}
+
+  openNewTicketForm() {
+    this.openForm.emit();
+  }
+
+  getNewId(): number {
+    return this.tickets.length > 0 ? Math.max(...this.tickets.map(ticket => ticket.id)) + 1 : 1;
+  }
+
+  addNewTicket(newTicket: Ticket) {
+    newTicket.id = this.getNewId();
+    this.tickets.push(newTicket);
+    this.showForm = false;
+  }
+
+  closeForm(): void {
+    this.showForm = false;
+  }
 }
