e7d0b44deddb80823f8d267010cebec78415e9cf
Edwin Chang <u20424575@tuks.co.za>
2023-06-23 12:12:24 +0200

    ✅ Setup notificationsDB config

---
+++ backend/src/configs/notificationsDB.config.ts
@@ -0,0 +1,24 @@
+import { ConnectOptions, connect, connection, Connection } from 'mongoose';
+
+let db: Connection;
+
+export const dbConnection = async () => {
+  if (!db) {
+    await connect(process.env.MONGO_USER_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+
+    db = connection.useDb('notificationsDB'); 
+
+    db.once('open', () => {
+      console.log('Connected to notificationsMongoDB');
+    });
+
+    db.on('error', (error: any) => {
+      console.error('An error occurred while connecting to MongoDB', error);
+    });
+  }
+
+  return db;
+};
