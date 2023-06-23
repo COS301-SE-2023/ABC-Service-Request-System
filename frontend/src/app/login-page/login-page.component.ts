0827f5f912c7b527ee4ca8c4d412d7ca846c2715
Jaimen789 <jaimengovender26@gmail.com>
2023-06-23 10:48:14 +0200

    🚀 Authentication complete

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
+import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -10,7 +12,7 @@ export class LoginPageComponent {
   password = '';
   email = '';
 
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService, private authService: AuthService) { }
 
   navigateToDashboard(event: Event) {
     event.preventDefault();
@@ -18,10 +20,18 @@ export class LoginPageComponent {
   }
 
   loginUser(): void {
-    alert("email is: " + this.email + " pass is: " + this.password);
-      this.userService.loginUser({ emailAddress: this.email, password: this.password }).subscribe({
-        next: (response: any) => console.error(response),
-        error: (error: any) => console.error(error),
-      });
-    }
+    // alert("email is: " + this.email + " pass is: " + this.password);
+    this.userService.loginUser({ emailAddress: this.email, password: this.password }).subscribe({
+      next: (response: any) => {
+        console.log('Login response:', response); // Log the response for debugging
+        const token = response.token; // Assuming the token is returned in the response
+        this.authService.decodeToken(token);
+        console.log('Role:', this.authService.getRole()); // Log the role for debugging
+        
+        // Navigate to dashboard
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error: any) => console.error(error),
+    });
+  }
 }
