974bc2694e92f440483829d1025c1b7784035cc9
Edwin Chang <u20424575@tuks.co.za>
2023-06-23 13:02:21 +0200

    ✅ Added Notifications model

---
+++ backend/src/models/notifications.model.ts
@@ -0,0 +1,30 @@
+import { Schema, connection, model } from "mongoose"
+
+export interface notifications {
+    notificationType: string;
+    creatorEmail: string;
+    assignedEmail: string;
+    ticketSummary: string;
+    link: string;
+}
+
+export const notificationsSchema = new Schema<notifications> (
+    {
+        notificationType: {type: String, required: true},
+        creatorEmail: {type: String, required: true},
+        assignedEmail: {type: String, required: true},
+        ticketSummary: {type: String, required: true},
+        link: {type: String, required: true}
+    },{
+        toJSON: {
+            virtuals: true
+        },
+        toObject: {
+            virtuals: true
+        },
+        timestamps: true
+    }
+);
+
+const notificationsDB = connection.useDb("NotificationsDB");
+export const NotificationsModel = notificationsDB.model("notifications", notificationsSchema)
