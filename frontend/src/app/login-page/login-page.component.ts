10864bf5ba8ab64c9c69ab2ad971db3d8db18a0a
Jaimen789 <jaimengovender26@gmail.com>
2023-06-23 12:47:44 +0200

    🎨 Updated file structure and files for testing

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService } from '../user.service';
+import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
 
 @Component({
