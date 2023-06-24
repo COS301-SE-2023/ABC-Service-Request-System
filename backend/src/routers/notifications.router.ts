277e23226e16b6d8e0c682a58f99a8372667cab7
Edwin Chang <u20424575@tuks.co.za>
2023-06-24 14:44:56 +0200

    ✅ Added Seed data

--- backend/src/routers/notifications.router.ts
+++ backend/src/routers/notifications.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import expressAsyncHandler from "express-async-handler";
 import { NotificationsModel } from "../models/notifications.model";
+import { sample_notifications } from "../sampleNotifications";
 
 const router = Router();
 
@@ -8,6 +9,21 @@ router.get('/', expressAsyncHandler(
     async (req, res) => {
         const notifications = await NotificationsModel.find();
         res.send(notifications);
+    }
+));
+
+router.post('/seed', expressAsyncHandler(
+    async (req, res) => {
+        const notificationsCount = await NotificationsModel.countDocuments();
+        if(notificationsCount > 0){
+            res.status(400).send("Seed is already done");
+            return;
+        }
+
+        NotificationsModel.create(sample_notifications)
+            .then(data => {res.status(201).send(data)})
+            .catch(err => {res.status(500).send({message: err.message}); });
+        // res.status(200).send("Seed is done!");
     }
 ));
 
