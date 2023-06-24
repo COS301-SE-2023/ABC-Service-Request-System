68715db0401b23e0b82ca000440deabcd9703e04
raamNnoodlez <ashirbutt998@gmail.com>
2023-06-24 19:08:46 +0200

    Workflows working

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -190,7 +190,6 @@ router.post("/create_user", expressAsyncHandler(
             transporter.sendMail(mailOptions, function(error, info) {
                 if (error) {
                     console.log(error);
-                    res.status(400).send("Email send error");
                 } else {
                     console.log("Email sent: " + info.response);
                 }
