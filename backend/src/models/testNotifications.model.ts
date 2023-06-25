66f424e7d2b24d10f813e096f684ee3dee9c49f1
Edwin Chang <u20424575@tuks.co.za>
2023-06-25 23:41:22 +0200

    🚧 Initialised models for testing

---
+++ backend/src/models/testNotifications.model.ts
@@ -0,0 +1,31 @@
+import { Schema, connection, model } from "mongoose"
+
+export interface notifications {
+    notificationType: string;
+    creatorEmail: string;
+    assignedEmail: string;
+    ticketSummary: string;
+    link: string;
+}
+
+export const notificationsSchema = new Schema<notifications> (
+    {
+        notificationType: {type: String, required: true},
+        creatorEmail: {type: String, required: true},
+        assignedEmail: {type: String, required: true},
+        ticketSummary: {type: String, required: true},
+        link: {type: String, required: true}
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        toObject: {
+            virtuals: true
+        },
+        timestamps: true
+    }
+);
+
+const notificationsDB = connection.useDb("test");
+export const TestNotificationsModel = notificationsDB.model("notifications", notificationsSchema)
