8c8480aae0b2c68df595782eb47f3981e044cad6
Jaimen789 <jaimengovender26@gmail.com>
2023-06-26 15:06:15 +0200

    Unit test

--- frontend/src/services/user.service.spec.ts
+++ frontend/src/services/user.service.spec.ts
@@ -1,13 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
     service = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify(); // Verify that no unmatched requests are outstanding
   });
 
   it('should be created', () => {
