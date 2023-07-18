01bff92138825deeda147dabc04a5506d6d085d6
Priyul <Priyul20@gmail.com>
2023-07-18 23:53:55 +0200

    🚀 header & profile image previews

--- frontend/src/app/settings-profile/settings-profile.component.ts
+++ frontend/src/app/settings-profile/settings-profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { UserService } from 'src/services/user.service';
 import { user } from '../../../../backend/src/models/user.model';
 import { AuthService } from 'src/services/auth.service';
 import { GroupService } from 'src/services/group.service';
+import { group } from '../../../../backend/src/models/group.model';
 @Component({
   selector: 'app-settings-profile',
   templateUrl: './settings-profile.component.html',
@@ -10,10 +11,66 @@ import { GroupService } from 'src/services/group.service';
 })
 export class SettingsProfileComponent { // implements OnInit{
 
+  currentUser!: user;
+  groupIds: string[] = [];
+  groups: group[] = [];
+  headerPhoto = '../../assets/bimg.jpg';
+
   constructor(private userService: UserService, private authService: AuthService,
   private groupService: GroupService) {}
 
-  // ngOnInit() {
+  ngOnInit(): void {
+    this.currentUser = this.authService.getUser();
+    this.groupIds = this.authService.getUser().groups;
+    if (this.currentUser) {
+      this.currentUser.groups.forEach(groupId => {
+        this.groupService.getGroupById(groupId).subscribe(group => {
+          this.groups.push(group);
+        });
+      });
+    }
+  }
 
-  // }
+  @ViewChild('fileUploader')
+  fileUploader!: ElementRef;
+
+  onFileSelected(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files[0]) {
+      const file = target.files[0];
+
+      // Show preview
+      const reader = new FileReader();
+      reader.onload = e => {
+        if (typeof reader.result === 'string') {
+          this.currentUser.profilePhoto = reader.result;
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  @ViewChild('headerFileUploader')
+  headerFileUploader!: ElementRef;
+
+  onHeaderFileSelected(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files[0]) {
+      const file = target.files[0];
+
+      // Show preview
+      const reader = new FileReader();
+      reader.onload = e => {
+        if (typeof reader.result === 'string') {
+          // this.currentUser.headerPhoto = reader.result;
+          this.headerPhoto = reader.result;
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+
 }
