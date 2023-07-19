4f47b2f1ff1db420cdac996c36996af56f0eff74
Jhindosh <dharshanpillay@gmail.com>
2023-07-20 00:41:49 +0200

    notifications-search-component

---
+++ frontend/src/app/notifications-search/notifications-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-search',
+  templateUrl: './notifications-search.component.html',
+  styleUrls: ['./notifications-search.component.scss']
+})
+export class NotificationsSearchComponent {
+
+}
