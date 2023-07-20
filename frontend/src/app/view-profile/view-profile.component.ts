fb8222bb3699b0198f6a7c30dd269c235511ba7f
Priyul <Priyul20@gmail.com>
2023-07-20 10:55:20 +0200

    🚑 view-profile frontend + backend

--- frontend/src/app/view-profile/view-profile.component.ts
+++ frontend/src/app/view-profile/view-profile.component.ts
@@ -3,97 +3,65 @@ import { UserService } from 'src/services/user.service';
 import { user } from '../../../../backend/src/models/user.model';
 import { AuthService } from 'src/services/auth.service';
 import { Chart, registerables } from 'chart.js';
-Chart.register(...registerables);
+import { GroupService } from 'src/services/group.service';
+import { group } from '../../../../backend/src/models/group.model';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-view-profile',
   templateUrl: './view-profile.component.html',
   styleUrls: ['./view-profile.component.scss']
 })
 export class ViewProfileComponent implements OnInit {
-  firstTimeName!: string;
-  firstTimeSurname!:string;
-  user!: user;
-  profilePicture!: File;
-  backgroundPicture!: File;
-  userPic!: string;
 
-  constructor(private userService: UserService, private authService: AuthService) {}
+  constructor(private userService: UserService,
+    private authService: AuthService,
+    private groupService: GroupService,
+    private route: ActivatedRoute) {}
 
-  getUser(userId: string) {
+    currentUser!: user;
+    tempUser!:user;
+    userBio?:string;
+    githubLink?:string;
+    linkedinLink?:string;
+    groups: group[] = [];
 
-    this.userService.getUser(userId).subscribe(
-      (user: user) => {
-        this.user = user;
-        //console.log(this.user);
-        this.firstTimeName = user.name;
-        this.firstTimeSurname = user.surname;
-      },
+  ngOnInit() {
+    // this.authService.getUserObject().subscribe(
+    //   (result: any) => {
+    //     this.currentUser = result;
+    //     this.userBio = this.currentUser.bio;
+    //     this.githubLink = this.currentUser.github;
+    //     this.linkedinLink = this.currentUser.linkedin;
+    //   }
+    // );
 
-      (error: any) => {
-        console.error(error);
+    // this.tempUser = this.authService.getUser();
+    // if (this.tempUser) {
+    //   this.tempUser.groups.forEach(groupId => {
+    //     this.groupService.getGroupById(groupId).subscribe(group => {
+    //       this.groups.push(group);
+    //     });
+    //   });
+    // }
+    this.route.queryParams.subscribe(params => {
+      const id = params['id'];
+      console.log(id);
+      if (id) {
+        this.userService.getUser(id).subscribe(user => {
+          this.currentUser = user;
+          this.userBio = this.currentUser.bio;
+          this.githubLink = this.currentUser.github;
+          this.linkedinLink = this.currentUser.linkedin;
+          this.currentUser.groups.forEach(groupId => {
+            this.groupService.getGroupById(groupId).subscribe(group => {
+              this.groups.push(group);
+            });
+          });
+        });
       }
-    );
+    });
 
   }
 
-  ngOnInit() {
-    const userId = this.getUserObject().id; // Replace with the actual user ID
-    this.getUser(userId);
-   console.log("hi");
-   this.RenderChart();
-   this.userService.getUser(userId).subscribe((user: user)=>{
-   this.user = user;
-   });
-
-   this.userPic = this.getUsersProfilePicture();
- }
-
- getUsersProfilePicture(){
-  const user = this.authService.getUser();
-  console.log("userrr", user);
-  console.log("profile photo: " + user.profilePhoto);
-  return this.user.profilePhoto;
-}
-
-getUsersBackgroundPicture(){
-  // const user = this.authService.getUser();
-  // console.log("userrr", user);
-  // console.log("background photo: " + user.backgroundPhoto);
-  // return this.user.BackgroundPhoto;
-}
-
-getUsersBio(){
-  // const user = this.authService.getUser();
-  // console.log("userrr", user);
-  // console.log("bio: " + user.bio);
-  // return this.user.bio;
-}
-
-getUserObject(){
-  return this.authService.getUser();
-}
-
-
-
-RenderChart() {
-  const myChart = new Chart("piechart", {
-    type: 'bar',
-    data: {
-      labels: ['Jan', 'Feb', 'Mar', 'April', 'May'],
-      datasets: [{
-        label: '# of tickets completed',
-        data: [12, 10, 17, 20, 5],
-        backgroundColor: 'rgba(54, 162, 235, 0.5)', // Optional: Add a background color to the bars
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-
-}
 }
