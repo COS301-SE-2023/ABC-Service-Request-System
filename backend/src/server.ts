c18d8af0e96131fde5d27046416ca417a4d52a8a
Priyul <Priyul20@gmail.com>
2023-07-30 20:21:59 +0200

    ✨ server credentials bug fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,6 +28,12 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+  next();
+});
+
 
 // app.use('/api/ticket', createProxyMiddleware({ 
 //   target: 'http://localhost:3001', 
