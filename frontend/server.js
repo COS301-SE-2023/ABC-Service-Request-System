7dfa8aacb0b12c285a490afa242fdfdbc3added3
raamNnoodlez <ashirbutt998@gmail.com>
2023-07-30 18:26:13 +0200

    server

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const path = require('path');
 
+const fs = require('fs');
+
+function printDirectoryStructure(directory) {
+  const files = fs.readdirSync(directory);
+  files.forEach(file => {
+    const filePath = path.join(directory, file);
+    const stats = fs.statSync(filePath);
+    if (stats.isDirectory()) {
+      console.log(`Directory: ${filePath}`);
+      printDirectoryStructure(filePath);
+    } else {
+      console.log(`File: ${filePath}`);
+    }
+  });
+}
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, './dist/project')));
 
 app.get('*', (req, res) => {
+  printDirectoryStructure(__dirname);
   res.sendFile(path.resolve(__dirname, './dist/project/index.html'));
 });
 
